d9bd007b9dab9316d67567442d14181e28ea8e04
Елена <tyshenko61@mail.ru>
2020-01-07 00:37:33 +0500

    Определение структур данных

--- src/components/trip-info.js
+++ src/components/trip-info.js
@@ -1,10 +1,54 @@
 // Маршрут
-export const createTripInfoTemplate = () => {
+import {formatInfoDate} from "../utils";
+const listCyties = (points) => {
+  const str = [`Ekaterinburg`];
+  points.map((point) => {
+    if (point.city) {
+      str.push(point.city);
+    }
+  });
+  return str;
+};
+const strPoints = (arr) => {
+  const last = arr.slice(-1);
+  switch (arr.length) {
+    case 1: return `${arr[0]} ... ${arr[0]}`;
+    case 2: return `${arr[0]} &mdash; ${arr[1]}`;
+    case 3: return `${arr[0]} &mdash; ${arr[1]} &mdash; ${arr[2]}`;
+    default: return `${arr[0]} ... ${last}`;
+  }
+};
+const dopCost = (point) => {
+  let sum = 0;
+  const {offers} = point;
+  offers.map((it) => {
+    if (it.isCheck) {
+      sum += it.cost;
+    }
+  });
+  return sum;
+};
+export const allCost = (points) => {
+  let cost = 0;
+  points.map((point) => {
+    cost += point.cost;
+    cost += dopCost(point);
+  });
+  return cost;
+};
+export const createTripInfoTemplate = (points) => {
+  const duration = formatInfoDate(points[0].date[0], points[points.length - 1].date[1]);
+  const list = listCyties(points);
+  const str = strPoints(list);
+  const cost = allCost(points);
+  const tripInfoCost = document.querySelector(`.trip-info__cost span`);
+  tripInfoCost.innerHTML = `${cost}`;
   return (
     `<div class="trip-info__main">
-              <h1 class="trip-info__title">Amsterdam &mdash; ... &mdash; Amsterdam</h1>
-
-              <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;21</p>
-           </div>`
+        <h1 class="trip-info__title">${str}</h1>
+        <p class="trip-info__dates">${duration}</p>
+     </div>`
   );
 };
+
+
---
+++ src/utils.js
@@ -0,0 +1,87 @@
+const MONTHNAMES = [
+  `JAN`,
+  `FEB`,
+  `MAR`,
+  `APR`,
+  `MAY`,
+  `JUN`,
+  `JUL`,
+  `AUG`,
+  `SEP`,
+  `OCT`,
+  `NOV`,
+  `DEC`,
+];
+export const formatDateTime = (date) => {
+  const time = formatTime(date);
+  const day = date.getDate();
+  const dayDate = day < 10 ? `0${day}` : `${day}`;
+  const month = date.getMonth() + 1;
+  const monthDate = month < 10 ? `0${month}` : `${month}`;
+  const year = date.getFullYear().toString(10).slice(2);
+  return `${dayDate}/${monthDate}/${year} ${time}`;
+};
+
+export const formatDate = (date) => {
+  const day = date.getDate();
+  const dayDate = day < 10 ? `0${day}` : `${day}`;
+  const month = date.getMonth() + 1;
+  const monthDate = month < 10 ? `0${month}` : `${month}`;
+  const year = date.getFullYear().toString(10).slice(2);
+  return `${dayDate}/${monthDate}/${year}`;
+};
+
+export const formatTime = (date) => {
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const hoursDate = hours < 10 ? `0${hours}` : `${hours}`;
+  const minutesDate = minutes < 10 ? `0${minutes}` : `${minutes}`;
+  return `${hoursDate}:${minutesDate}`;
+};
+export let duration;
+duration = (date1, date2) => {
+  const diffDate = date2 - date1;
+  let minutes = Math.round(diffDate / (60 * 1000));
+  let hours = Math.floor(minutes / 60);
+  let day = Math.floor(hours / 24);
+  minutes -= hours * 60;
+  hours -= day * 24;
+  if (day === 0) {
+    day = ``;
+  } else if (day >= 9) {
+    day = `${day}D `;
+  } else {
+    day = `0${day}D `;
+  }
+  if (!day) {
+    if (hours === 0) {
+      hours = ``;
+    } else if (hours < 9) {
+      hours = `0${hours}H `;
+    } else {
+      hours = `${hours}H `;
+    }
+  } else {
+    hours = (hours < 9) ? `0${hours}H ` : `${hours}H `;
+  }
+
+  minutes = (minutes < 9) ? `0${minutes}M` : `${minutes}M`;
+  return `${day}${hours}${minutes}`;
+};
+export const formatInfoDate = (date1, date2) => {
+  const month1 = MONTHNAMES[date1.getMonth()];
+  const month2 = MONTHNAMES[date2.getMonth()];
+  const day1 = date1.getDate();
+  const day2 = date2.getDate();
+  if (date1.getMonth() > date2.getMonth()) {
+    return `${day1} ${month1} &mdash;&nbsp;${day2} ${month2} `;
+  } else {
+    return `${month1} ${day1} &mdash;&nbsp;${day2}`;
+  }
+};
+export const formatDateList = (date) => {
+  const num = parseInt(date.slice(3, 5), 10) - 1;
+  const month = MONTHNAMES[num];
+  const day = parseInt(date.slice(0, 2), 10);
+  return `${month} ${day}`;
+};
